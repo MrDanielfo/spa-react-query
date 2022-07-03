02f0f46a0b8213fa3df031b9f428dfa7c58bc24d
Josué Daniel Flores Morales <mrdanielfo@MacBook-Pro-de-Josue.local>
2022-07-03 15:13:03 -0500

    useful files added

---
+++ src/components/appointments/hooks/useCancelAppointment.ts
@@ -0,0 +1,45 @@
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+
+import { Appointment } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
+
+// for when server call is needed
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+  const patchData = [{ op: 'remove', path: '/userId' }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
+
+// TODO: update return type
+export function useCancelAppointment(): UseMutateFunction<
+  void,
+  unknown,
+  Appointment,
+  unknown
+> {
+  const toast = useCustomToast();
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(
+    (appoinment: Appointment) => removeAppointmentUser(appoinment),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([queryKeys.appointments]);
+        toast({
+          title: 'You have cancelled the appoinment',
+          status: 'success',
+        });
+      },
+    },
+  );
+
+  // TODO: replace with mutate function
+  // return (appointment: Appointment) => {
+  //   // nothing to see here
+  // };
+
+  return mutate;
+}
---
+++ src/components/staff/hooks/useStaff.ts
@@ -0,0 +1,36 @@
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import { useQuery } from 'react-query';
+
+import type { Staff } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+import { filterByTreatment } from '../utils';
+
+// for when we need a query function for useQuery
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
+
+interface UseStaff {
+  staff: Staff[];
+  filter: string;
+  setFilter: Dispatch<SetStateAction<string>>;
+}
+
+export function useStaff(): UseStaff {
+  // for filtering staff by treatment
+  const fallback = [];
+  const [filter, setFilter] = useState('all');
+
+  const filterFn = useCallback((data) => filterByTreatment(data, filter), [
+    filter,
+  ]);
+
+  // TODO: get data from server via useQuery
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff, {
+    select: filter === 'all' ? undefined : filterFn,
+  });
+
+  return { staff, filter, setFilter };
+}
